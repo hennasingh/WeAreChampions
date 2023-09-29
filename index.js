6b5dcd97948349e83faec3709d86b2f4e7aa3c8b
Henna Singh <hennasingh04@gmail.com>
2023-09-29 21:16:48 +0100

    added firebase dependencies and initialized app to save data to realtime firebase

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://we-are-champions007-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const endorsementsInDB = ref(database, "endorsements");
+
 const textAreaEl = document.getElementById("msg-text");
 const msgFromEl = document.getElementById("msg-from");
 const msgToEl = document.getElementById("msg-to");
@@ -10,8 +21,21 @@ btnPublish.addEventListener('click', function() {
     let msgTo = msgToEl.value;
 
     console.log(`To ${msgTo}, ${msg}, From ${from}`);
-    appendMsgToList(msg, from, msgTo);
+    const endorsement = {
+        msgFrom: from,
+        to: msgTo,
+        message: msg
+    }
+    push(endorsementsInDB, endorsement);
+
+    clearInputFields();
 })
+
+function clearInputFields() {
+    textAreaEl.value = "";
+    msgFromEl.value = "";
+    msgToEl.value= "";
+}
 
 
 function appendMsgToList(msg, msgFrom, msgTo) {
@@ -19,6 +43,7 @@ function appendMsgToList(msg, msgFrom, msgTo) {
     let newMsgEL = document.createElement("p");
     let newFrmEl = document.createElement("p");
     let newToEl = document.createElement("p");
+    let newLikeEl = document.createElement("i");
 
     newToEl.textContent = `To ${msgTo}`;
     newToEl.className ='to';
@@ -27,6 +52,10 @@ function appendMsgToList(msg, msgFrom, msgTo) {
 
     newFrmEl.textContent = `From ${msgFrom}`;
     newFrmEl.className = 'from'
+
+    newLikeEl.textContent = ` 0`
+    newLikeEl.className = 'fa fa-heart';
+    newFrmEl.append(newLikeEl);
 
     newEl.append(newToEl, newMsgEL, newFrmEl);
     msgListEL.append(newEl);
